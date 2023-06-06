28d160f6424840cb4344fe6b1df1dbd0cf8d1f9d
Kemal Ilkilic <kemalonur.ilkilic@bahcesehir.edu.tr>
2023-06-06 15:28:08 +0300

    Create Angular App

---
+++ frontend/src/app/shared/models/Cart.ts
@@ -0,0 +1,13 @@
+import { CartItem } from "./CartItem";
+
+export class Cart {
+  items: CartItem[] = [];
+
+  get totalPrice(): number {
+    let sum = 0;
+    this.items.forEach(item => {
+      sum += item.price;
+    });
+    return sum;
+  }
+}
